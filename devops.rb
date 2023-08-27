1352335953a27ea1efdaf76dcc910519157dae37
Gaurav Sablok <sablokg@gmail.com>
2023-08-27 15:13:33 +0530

    Create devops.rb

---
+++ devops.rb
@@ -0,0 +1,19 @@
+def rubyServer(dir_path, backup_dir_path, file_extension)
+=begin
+A ruby function to make server backups when you are
+dealing with large amount of the sequencing data on
+a high throughput cluster. run this with the dir_path
+as your path of the existing files and the backup dir
+as the path where you want to copy the files and the
+extension of the files for which you want to copy
+=end
+        dir_storage = Dir.open(dir_path)
+        dir_path = backup_dir_path.to_s
+        file_storage_copy = dir_storage.each{|each| \
+                        puts "cp -r " + dir_path + "/" + "backup" + each \
+                                          if each.end_with?(file_extension)}
+        file_storage_move = dir_storage.each{|each| \
+                            puts "mv " + dir_path + "/" + "backup" + each \
+                                                if each.end_with?(file_extension)}
+    return file_storage
+end
